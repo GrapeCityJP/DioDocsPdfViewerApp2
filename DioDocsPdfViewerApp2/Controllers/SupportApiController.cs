1e9a24de3b729aa7353580d191daf8756cc65b4f
Kuni <kuni.ujiie@grapecity.com>
2021-05-28 17:32:34 +0900

    プロジェクト ファイルを追加します。

---
+++ DioDocsPdfViewerApp2/Controllers/SupportApiController.cs
@@ -0,0 +1,12 @@
+﻿using SupportApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DioDocsPdfViewerApp2.Controllers
+{
+    [Route("api/pdf-viewer")]
+    [ApiController]
+    public class SupportApiController : GcPdfViewerController
+    {
+
+    }
+}
